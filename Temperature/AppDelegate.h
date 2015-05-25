414a83076cd97c9f333695161881512b97aa630d
Matt Fenwick <matt.fenwick@hudl.com>
2015-05-25 09:41:04 -0500

    Initial Commit

---
+++ Temperature/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Temperature
+//
+//  Created by Matthew Fenwick on 5/25/15.
+//  Copyright (c) 2015 Matthew Fenwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
